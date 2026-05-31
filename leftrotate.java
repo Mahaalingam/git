44444f47021a51f608eeb3cfc66bf20bd98fcd4c
mahaa <mahaalingam781@gmail.com>
2026-05-31 11:49:32 +0530

    java practice

---
+++ leftrotate.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class leftrotate{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int arr1[]=new int[n];
+        int ind=0;
+        for(int i=k;i<n;i++){
+            arr1[ind++]=arr[i];
+        }
+        for(int i=0;i<k;i++){
+            arr1[ind++]=arr[i];
+        }
+        System.out.println(Arrays.toString(arr1));
+        sc.close();
+    }
+}
